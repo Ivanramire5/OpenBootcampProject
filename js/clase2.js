5a5567b6bc3cd72a9d3278f7128feef6b650ddd8
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-05-11 21:19:57 -0300

    segundo modulo

---
+++ js/clase2.js
@@ -0,0 +1,20 @@
+
+var nombre = "Iván Ramirez";
+console.log(nombre)
+
+var edad = 24;
+console.log(edad)
+
+var profesion = true;
+console.log("¿Eres desarrollador?" + " " + profesion)
+
+const fechaDeNacimiento = new Date (1999, 1, 5)
+console.log(fechaDeNacimiento)
+
+const libro = {
+    nombre: "La llamada de Ctulhu",
+    autor: "H.P Lovecraft",
+    fecha: "Febrero de 1928",
+    url: "https://ciudadseva.com/texto/la-llamada-de-cthulhu/"
+}
+console.log(libro)
