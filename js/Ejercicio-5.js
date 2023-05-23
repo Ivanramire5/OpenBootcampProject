9d258307d17b3ea47d15802278607149955cf6df
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-05-23 17:36:29 -0300

    quinta entrega

---
+++ js/Ejercicio-5.js
@@ -0,0 +1,31 @@
+
+// Una variable que contenga tu altura en centímetros (entero)
+
+var alturaEnCm = 1.69
+console.log(alturaEnCm)
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+
+var miAlturaEnMetros = 1.69
+console.log(parseFloat(miAlturaEnMetros))
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+var miPeso = 63.5
+console.log(parseFloat(miPeso))
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+
+var miAltura = 1.69
+console.log(Math.ceil(miAltura))
+
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+var peso = 63.5
+console.log(Math.floor(peso))
+
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+var contenido = Number.MAX_VALUE;
+console.log(contenido)
